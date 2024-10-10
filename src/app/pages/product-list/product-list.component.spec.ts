ada704f2949cdfeb18bc66bf7deb253eaebbe511
AYALA ESPINOZA MARTHA MERCEDES <mmayalae@pichincha.com>
2024-10-10 11:48:41 -0500

    feat: update testing

--- src/app/pages/product-list/product-list.component.spec.ts
+++ src/app/pages/product-list/product-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductService } from '../../services/product.service';
@@ -10,51 +10,53 @@ import { TableComponent } from '../../components/table/table.component';
 import { FormsModule } from '@angular/forms';
 
 
+const mockResponse:ResponseProduct = { data:[{
+  id: "1",
+  name: "Cobro de Cheque",
+  description: "Es una transacción  realizada en ventanilla",
+  logo: "https://tse4.mm.bing.net/th/id/OIP.JQF5ttuSCKCxXp7iHME9TgAAAA?w=442&h=332&rs=1&pid=ImgDetMain",
+  date_release: new Date("2024-02-01"),
+  date_revision: new Date ("2024-02-04"),
+  }]
+};
+
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
-  let productService:ProductService
   let httpTestingController:HttpTestingController
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ProductListComponent, HeaderComponent, TableComponent],
       imports: [HttpClientTestingModule, FormsModule],
-      providers: [ProductService],
+      providers:[ProductService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
-    productService = TestBed.inject(ProductService)
-    httpTestingController = TestBed.inject(HttpTestingController)
-    fixture.detectChanges();
+    httpTestingController = TestBed.inject(HttpTestingController);
+    
   });
 
   afterEach(() =>{
-    httpTestingController.verify()
-  })
+     httpTestingController.verify()
+   })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it("should call getAllProducts and return a list of products", () => {
-    const mockResponse:ResponseProduct = { data:[{
-      id: "1",
-      name: "Cobro de Cheque",
-      description: "Es una transacción  realizada en ventanilla",
-      logo: "https://tse4.mm.bing.net/th/id/OIP.JQF5ttuSCKCxXp7iHME9TgAAAA?w=442&h=332&rs=1&pid=ImgDetMain",
-      date_release: new Date("2024-02-01"),
-      date_revision: new Date ("2024-02-04"),
-    },
-      
-    ]
-  };
-     
-    component.getDataProducts();
+  it("should call getAllProducts and return a list of products", fakeAsync(() => {  
+    fixture.detectChanges();
+    
+    const req = httpTestingController.expectOne("http://localhost:3001/bp/products")
+    req.flush(mockResponse);
+    tick();
+    
     expect(component.dataProducts).toEqual(mockResponse.data);
     expect(component.loading).toBe(false)
-  });
+  }));
+
 });
--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,24 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, tick } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ResponseProduct } from '../interfaces/product.interface';
 import { retryWhen } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
-
+const mockResponse: ResponseProduct ={
+  data:[{
+    id:'1',
+    name:'Credito',
+    logo:'',
+    description:'Credito de consumo',
+    date_release: new Date('2024-09-10'),
+    date_revision:new Date('2025-09-10')
+  }]
+}
 describe('ProductService', () => {
-  let service: ProductService;
+  let service:ProductService;
   let httpTestingController:HttpTestingController;
-  const URL_BASE ='https://api.example.com/'
+  const URL_BASE ='http://localhost:3001/bp/'
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [ ProductService ],
     });
-    
-    service = TestBed.inject(ProductService);
-    httpTestingController: TestBed.inject(HttpTestingController)
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(ProductService)
   });
 
   afterEach(() =>{
@@ -26,33 +36,21 @@ describe('ProductService', () => {
   })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(ProductService).toBeTruthy();
   }); 
 
   it('should call the correct URL and send the right headers', () =>{
-    const mockResponse: ResponseProduct ={
-      data:[{
-        id:'1',
-        name:'Credito',
-        logo:'',
-        description:'Credito de consumo',
-        date_release: new Date('2024-09-10'),
-        date_revision:new Date('2025-09-10')
-      }]
-    }
-
-    service.getAllProducts().subscribe((response) =>{
-      expect(response).toEqual(mockResponse)
-    })
-
-    const req = httpTestingController.expectOne(URL_BASE + 'products')
+ 
+    const req = httpTestingController.expectOne(URL_BASE + 'pruducts')
+    req.flush(mockResponse)
+   
 
     expect(req.request.method).toEqual('GET')
-
     expect(req.request.headers.has('authorId')).toBe(true)
     expect(req.request.headers.get('authorId')).toBe('50');
 
-    req.flush(mockResponse)
+    
     
   })
+  
 });
