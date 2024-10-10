dc8ac20dfaf377a1a24e5bb5ab3dcd4fbc3c662f
AYALA ESPINOZA MARTHA MERCEDES <mmayalae@pichincha.com>
2024-10-10 14:16:45 -0500

    feat: add routes in component app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 
 
 
-const routes: Routes = [
-  {path:"",component:ProductListComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 
 describe('AppComponent', () => {
-
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports:   [RouterTestingModule],
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ProductListComponent
       ],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    expect(component).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
+
+  // it(`should have as title 'capacitacion-frontend'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('capacitacion-frontend');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('capacitacion-frontend app is running!');
+  // });
 });
